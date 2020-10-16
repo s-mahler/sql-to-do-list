dddc3c26dc2fe91f6a862f82b2c399024cc0ae3e
Sam Mahler <mahler.sam@gmail.com>
2020-10-16 16:56:01 -0500

    404 error solved in server.js

--- server/server.js
+++ server/server.js
@@ -6,6 +6,9 @@ const app = express();
 app.use( bodyParser.urlencoded({ extended: true }));
 app.use(express.static('server/public'));
 
+let listRouter = require('./routes/list_route');
+app.use('/list', listRouter);
+
 const PORT = 5000;
 app.listen(PORT, () => {
     console.log('up and running on port', PORT);
